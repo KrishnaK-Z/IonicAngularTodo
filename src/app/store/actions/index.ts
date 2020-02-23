f787f5a9ce095c961632a01c14aee9996516d245
Ninja_Avenger <31925115+KrishnaK-Z@users.noreply.github.com>
2020-02-23 19:40:22 +0530

    Add files via upload

---
+++ src/app/store/actions/index.ts
@@ -0,0 +1,17 @@
+import { Task } from '../../model/task';
+
+export class AddTask {
+    static readonly type = '[Tasks] Add Task';
+
+    constructor(public payload: Task) {}
+}
+
+export class FetchTasks {
+    static readonly type = '[Task] Fetch tasks';
+
+    constructor(public payload: {taskTitle: string}) {}
+}
+
+export class ClearState {
+    static readonly type = '[Tasks] Clear state';
+}
---
+++ src/app/store/state/index.ts
@@ -0,0 +1,49 @@
+import { State, Action, StateContext, Selector, NgxsOnInit } from '@ngxs/store';
+import { patch, append, removeItem, insertItem, updateItem } from '@ngxs/store/operators';
+import { tap, catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
+
+import { AddTask, FetchTasks, ClearState } from '../actions';
+
+
+import {Task} from '../../model/task';
+
+export class AppState {
+    readonly tasks: Task[]
+}
+
+@State<AppState>({
+    name: 'tasks',
+    defaults: {
+        tasks: []
+    }
+})
+
+export class TaskState implements NgxsOnInit {
+
+    @Selector()
+    static getTasks(state: AppState) {
+        return state.tasks;
+    }
+
+    ngxsOnInit(ctx: StateContext<AppState>) {
+        console.log('State initialized.');
+        ctx.dispatch(new ClearState());
+    }
+
+    @Action(ClearState)
+    clearState({ setState }: StateContext<AppState>) {
+        setState({
+            tasks: []
+    });
+    }
+
+    @Action(AddTask)
+    addTask({ getState, setState }: StateContext<AppState>, { payload }) {
+        const state = getState();
+        setState({
+            tasks: [...state.tasks, payload]
+        });
+    }
+
+}
